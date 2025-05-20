03a27debf312fa6d895cddf1db4e4548ece68c9a
游志信 <shin910113@wke.csie.ncnu.edu.tw>
2025-05-20 20:36:13 +0800

    feat(hardhat): add hardhat sepolia deploy config

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
